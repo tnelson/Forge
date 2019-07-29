26c972f2509d82676ae8357f1b86b9b3298e4d03
iambadatnames <charles_cutting@brown.edu>
2019-07-29 10:04:57 -0400

    Update info.rkt

--- forge/info.rkt
+++ forge/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "forge")
 
-(define deps '("forged-ocelot" "br" "rosette"))
+(define deps '("forged-ocelot" "beautiful-racket" "rosette"))
 
 (define scribblings '(("doc/quickstart.scrbl" ())))
