f488f00e1d972550008288da7106d201121e86f0
Tim Nelson <tn@cs.brown.edu>
2019-09-20 09:48:46 -0400

    update TTT boards

--- forge/examples/tttboards.rkt
+++ forge/examples/tttboards.rkt
@@ -5,10 +5,13 @@
 (declare-one-sig X #:extends Player)
 (declare-one-sig O #:extends Player)
 
-(declare-sig Index)
+(declare-sig Index ([inverse Index]))
 (declare-one-sig A #:extends Index)
 (declare-one-sig B #:extends Index)
 (declare-one-sig C #:extends Index)
+
+(pred fix-inverses
+      (= inverse (+ (-> A C) (-> B B) (-> C A))))
 
 (declare-sig Board ([places Index Index Player]))
 
@@ -43,7 +46,39 @@
                      (or (int= (card (join b (join (join places r) c))) 1)
                          (int= (card (join b (join (join places r) c))) 0))))))
 
+(pred (winH b p)
+      (some ([r Index])
+            (all ([c Index])
+                 ;(in b (join places p))))) ; TODO arity problem--error quality
+                 (in b (join (join (join places p) c) r)))))
+(pred (winV b p)
+      (some ([c Index])
+            (all ([r Index])
+                 (in b (join (join (join places p) c) r)))))
 
-;(run "xturn" (at-most-one-mark-per-square (some ([b Board]) (xturn b))) ((Player 2 2) (Index 3 3) (Board 1 1)))
-(run "oturn" (at-most-one-mark-per-square (some ([b Board]) (oturn b))) ((Player 2 2) (Index 3 3) (Board 1 1)))
+(pred (winD b p)
+      (all ([i Index])
+           (or 
+            (in b (join (join (join places p) i) i))
+            (in b (join (join (join places p) i) (join i inverse))))))
 
+(pred (winning b p)
+      (or (winH b p) (winV b p) (winD b p)))
+
+(pred (valid-board b)
+      (or (oturn b) (xturn b)))
+
+(run "xturn" (at-most-one-mark-per-square (some ([b Board]) (xturn b))) ((Player 2 2) (Index 3 3) (Board 1 1)))
+;(run "oturn" (at-most-one-mark-per-square (some ([b Board]) (oturn b))) ((Player 2 2) (Index 3 3) (Board 1 1)))
+(run "owinningDiag" (at-most-one-mark-per-square
+                     fix-inverses
+                     (some ([b Board]) (and (valid-board b) (winD b O))))
+     ((Player 2 2) (Index 3 3) (Board 1 1)))
+
+
+; pred management
+; error quality
+; UI + modes
+;  RUN ONE AT A TIME
+; functions
+; type annotations? check for *use*?
