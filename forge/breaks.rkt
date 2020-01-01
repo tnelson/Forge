9b6cde239cc8d844c2e4afc2217b2f43720b19da
asvarga <alexander_varga@alumni.brown.edu>
2020-01-01 02:16:14 -0500

    progress towards enforcing the criteria for sound strategy composition

--- forge/breaks.rkt
+++ forge/breaks.rkt
@@ -1,6 +1,9 @@
 #lang racket
 
 (require "lang/bounds.rkt" (prefix-in @ "lang/ast.rkt"))
+(require predicates)
+(require data/union-find)
+
 (provide constrain-bounds (rename-out [break-rel break]) break-bound break-formulas)
 
 ;;;;;;;;;;;;;;
@@ -19,11 +22,39 @@
 (struct break (sbound formulas) #:transparent)
 (define (make-break sbound [formulas (set)]) (break sbound formulas))
 
+; sigs  :: set<sig>
+; edges :: set<set<sig>> ; technically hyperedges
+(struct break-graph (sigs edges) #:transparent)
+
+; elem2class    :: A |-> uf-set<A>
+; broken        :: mut-set<A> ; set of broken canonical elements
+(struct eq-rel (elem2class broken))
+(define (make-eq-rel) (eq-rel (make-hash) (mutable-set)))
+(define (in-broken? e-r x) 
+    (define elem2class (eq-rel-elem2class e-r))
+    (define broken (eq-rel-broken e-r))
+    (define class (hash-ref elem2class x))
+    (define canon (uf-find class))
+    (set-member? broken canon)
+)
+; union two classes and mark as broken if either is broken
+(define (e-r-union! e-r c1 c2)
+    (define broken (eq-rel-broken e-r))
+    (define canon1 (uf-find c1))
+    (define canon2 (uf-find c2))
+    (define is-broken (or (set-member? broken canon1)
+                          (set-member? broken canon2)))
+    (uf-union! c1 c2)
+    (define canon3 (uf-find c1))
+    (when is-broken (set-add! broken canon3))
+)
+; (define (compose ))
+
 ; pri               :: Nat
-; get-broken-sigs   :: () -> set<sig>
+; break-graph       :: break-graph
 ; make-break        :: () -> break
 ; make-formulas     :: () -> set<formula>
-(struct breaker (pri get-broken-sigs make-break make-formulas) #:transparent)
+(struct breaker (pri break-graph make-break make-formulas) #:transparent)
 
 (define (bound->sbound bound) 
     (make-sbound (bound-relation bound)
@@ -194,7 +225,9 @@
                 (cond [broken
                     (set-union! formulas ((breaker-make-formulas breaker)))
                 ][else
-                    (define broken-sigs ((breaker-get-broken-sigs breaker)))
+                    (define break-graph (breaker-break-graph breaker))
+                    (define broken-sigs (break-graph-sigs break-graph))
+                    ; (define broken-sigs ((breaker-break-graph breaker)))
                     (cond [(subset? broken-sigs leaf-sigs)
                         (set-cons! candidates breaker)
                         (set! broken #t)
@@ -206,10 +239,54 @@
             (unless broken (set-cons! new-total-bounds bound))
         ])     
     )
+    (set! candidates (sort candidates < #:key breaker-pri))
 
-    ; TODO: avoid double breaking a sig
-    ; TODO: enforce forest criteria
+    #|
+        Now we try to use candidate breakers, starting with highest priority.
+        We maintain an equivalence class on sigs, with some classes being broken.
+        If applying a breaker would merge 2 broken classes or break a sig in a broken class,
+        the breaker isn't applied and the default formulas are used instead.
+        Otherwise, we do the break and update the equivalence classes.
+    |#
+
+    (define e-r (make-eq-rel))
+    (define sig2class (eq-rel-elem2class e-r))
+    (define broken-classes (eq-rel-broken e-r))
+    (for ([sig sigs]) (hash-set! sig2class sig (uf-new sig)))
+
     (for ([breaker candidates])
+        (define break-graph (breaker-break-graph breaker))
+        (define broken-sigs (break-graph-sigs break-graph))
+        (define broken-edges (break-graph-edges break-graph))
+
+        ; equivalence relation on equivalnce classes
+        (define e-r-2 (make-eq-rel))
+        (define canon2clocl (eq-rel-elem2class e-r-2))
+        (define broken-clocls (eq-rel-broken e-r-2))
+        (hash-for-each sig2class (λ (sig class) 
+            (hash-set! canon2clocl (uf-find class) (uf-new class)))) 
+
+        (for ([edge broken-edges])
+            (define clocl #f)
+            (define broken #f)
+            (for ([sig edge])
+                (when (set-member? broken-sigs sig) (set! broken #t))
+
+                (define class (hash-ref sig2class sig))
+                (define canon (uf-find class))
+                (define clocl2 (hash-ref canon2clocl canon))
+                (if clocl (e-r-union! clocl clocl2) (set! clocl clocl2))
+                (hash-set canon2clocl canon clocl)
+            )
+            (when broken (set-add! broken-clocls (uf-find clocl)))
+        )
+
+        ; TODO: for each clocl, if >1 of {its classes} ∪ {it} are broken, abort.
+        ;       otherwise, apply the break and squash the classes.
+        ;       if exactly 1 of those is broken, set the squashed class as broken.
+
+        (define acceptable #t)
+
         (define break ((breaker-make-break breaker)))
         (set-cons! new-total-bounds (break-bound break))
         (set-union! formulas (break-formulas break))
@@ -223,7 +300,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (add-strategy 'irref (λ (pri rel atom-lists rel-list) (breaker pri
-    (λ () (set))
+    (break-graph (set) (set))
     (λ () 
         (make-upper-break rel
                         (filter-not (lambda (x) (equal? (first x) (second x)))
@@ -231,7 +308,7 @@
     (λ () (set))
 )))
 (add-strategy 'ref (λ (pri rel atom-lists rel-list) (breaker pri
-    (λ () (set))
+    (break-graph (set) (set))
     (λ () 
         (make-lower-break rel
                         (filter     (lambda (x) (equal? (first x) (second x)))
@@ -240,7 +317,7 @@
     (λ () (set))
 )))
 (add-strategy 'linear (λ (pri rel atom-lists rel-list) (breaker pri
-    (λ () (set))
+    (break-graph (set) (set))
     (λ ()
         (define atoms (first atom-lists))
         (make-exact-break rel
@@ -248,7 +325,7 @@
     (λ () (set))
 )))
 (add-strategy 'acyclic (λ (pri rel atom-lists rel-list) (breaker pri
-    (λ () (set))
+    (break-graph (set) (set))
     (λ ()
         (define atoms (first atom-lists))
         (make-upper-break rel
@@ -259,7 +336,7 @@
     (λ () (set))
 )))
 (add-strategy 'tree (λ (pri rel atom-lists rel-list) (breaker pri
-    (λ () (set))
+    (break-graph (set) (set))
     (λ ()
         (define atoms (first atom-lists))
         (define rel2 (first rel-list))
@@ -298,7 +375,7 @@
 
 ; use to prevent breaks
 (add-strategy 'default (λ (pri rel atom-lists rel-list) (breaker pri
-    (λ () (set))
+    (break-graph (set) (set))
     (λ () 
         (make-upper-break rel (apply cartesian-product atom-lists)))
     (λ () (set))
