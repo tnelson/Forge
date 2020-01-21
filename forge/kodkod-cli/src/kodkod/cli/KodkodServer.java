8ebe0a4e8d9f3ad6aac23f6f2baa5e0b7bf92811
Luke West <lukewest73@gmail.com>
2020-01-21 11:55:10 -0500

    Fix unsat core display, and make kodkodserver always run the stepper problem

--- forge/kodkod-cli/src/kodkod/cli/KodkodServer.java
+++ forge/kodkod-cli/src/kodkod/cli/KodkodServer.java
@@ -376,7 +376,7 @@ public final class KodkodServer {
 					usage(1);
 				} else {
 					// Can't use a stepper problem when running kodkod on a file.
-					(new KodkodServer(incremental, false, fastParsing, errorOut)).serve(new File(args[i]));
+					(new KodkodServer(incremental, stepper, fastParsing, errorOut)).serve(new File(args[i]));
 					System.exit(0);
 				}
 			}
@@ -388,7 +388,7 @@ public final class KodkodServer {
         }
 
         System.err.println("Hey, no minisatprover error!");
-        
+
 		KodkodServer server = new KodkodServer(incremental, stepper, fastParsing, errorOut);
 		server.serve();
 	}
