de81fe5a949ca1ed2d6806612cc8742bebed67e2
asvarga <alexander_varga@alumni.brown.edu>
2020-01-26 15:57:55 -0500

    what am I doing

--- forge/examples/intTests.rkt
+++ forge/examples/intTests.rkt
@@ -1,18 +1,10 @@
 #lang forge
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 5b818b398cea100e6b68646a0ccab37a636aa1c6
 sig A {
     i: one Int
 }
 
-<<<<<<< HEAD
 run {
-    --some a1: A | some a2: A | a1.i > a2.i
-    3 < 4
+    some a1: A | a1.i = 3
+    --3 < 4
 } for exactly 4 A
-=======
-run {} for exactly 4 A
->>>>>>> 5b818b398cea100e6b68646a0ccab37a636aa1c6
