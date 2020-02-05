ce13fef398cdba7972a57a8edb874c2a8508253c
Tim Nelson <tbnelson@gmail.com>
2020-02-05 14:19:27 -0500

    a few more tests

--- forge/tests/basic/basicGraph.rkt
+++ forge/tests/basic/basicGraph.rkt
@@ -14,6 +14,7 @@ pred inNexus {
   all n: Node | some n2: Node | n->n2 in edges
 }
 
+
 test {inNexus} for exactly 5 Node expect sat
 test {} for 5 Node expect sat
 test {~edges != ~(~edges)} for 5 Node expect sat -- FLIP EXPECTATION TO GET PATTERN ERROR 
@@ -21,6 +22,11 @@ test {edges != ~(~edges)} for 5 Node expect unsat
 test {edges = ^edges} for 5 Node expect sat
 test {outNexus and not inNexus} for 5 Node expect sat
 test {#Node = 4} for exactly 5 Node expect unsat
--- test {#Node = 4} for 5 Node expect sat -- FAILING
+test {#Node = 4} for 5 Node expect sat
+test {*edges != ^edges + iden} for 5 Node expect unsat
+test {edges - (edges.edges + edges.edges.edges) != edges - edges.edges - edges.edges.edges} for 7 Node expect unsat
+test {some edges - univ->univ} for 5 Node expect unsat
+
+
 
 
