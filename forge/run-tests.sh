57c35ecbb41eb4f7af45580e3c29c9126f5c2db9
Mark Lavrentyev <lavrema@outlook.com>
2020-07-31 20:49:04 -0400

    Update run tests script

--- forge/run-tests.sh
+++ forge/run-tests.sh
@@ -3,36 +3,33 @@
 # Exit full script on interrupt
 trap "exit" INT
 
+# Check for args
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <test-directory>"
+    exit 1
+fi
+
 # Get test files
 testDir=$1
 testFiles="$(find $testDir -type f -name "*.rkt")"
 numTestFiles="$(echo "$testFiles" | wc -l)"
 
 # Helper variables
-_breakLine="-------------------------------------------\n"
-_numTestsCompleted=0
-_progressBarSize=40
+breakLine="-------------------------------------------\n"
+exitCode=0
 
 # Print header of test files found
-echo -e "Found the following $numTestFiles test files:\n$_breakLine$testFiles\n$_breakLine"
-
-# Helper functions
-repeatChar () {
-     echo -n "$(printf "$1%.0s" $(seq 1 $2))"
-}
-
-printBar () {
-    barSize="$((($_progressBarSize * $1) / $2))"
-    echo -ne "[$(repeatChar '█' $barSize)$(repeatChar ' ' $(($_progressBarSize - $barSize)))]"
-}
+echo -e "Found the following $numTestFiles test files:\n$breakLine$testFiles\n$breakLine"
 
 # Run tests and report progress
 for testFile in $testFiles; do
-    echo -e "\r$(printf "%-50.50s" "Running $testFile")"
-    printBar $_numTestsCompleted $numTestFiles
-
+    echo -e "\nRunning $testFile"
+    
     racket $testFile > /dev/null
 
-    ((_numTestsCompleted++))
+    if [[ $? ]]; then
+        exitCode=1
+    fi
 done
-#racket testFiles > /dev/null
+
+exit $exitCode
