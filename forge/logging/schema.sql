b74932021724c9d7996d2de9d8a402235b57d821
Thomas Del Vecchio <thomasdelvecchio4@gmail.com>
2020-08-13 10:01:48 -0400

    Fixed formatting of comments.

--- forge/logging/schema.sql
+++ forge/logging/schema.sql
@@ -1,28 +1,28 @@
 
-/* - Students
- *     - id
- *     - email e.g. "tdelvecc@cs.brown.edu"
+/* Students
+ *  - id
+ *  - email e.g. "tdelvecc@cs.brown.edu"
  */
 CREATE TABLE students (
     id SERIAL PRIMARY KEY,
     email TEXT
 );
 
-/* - Projects
- *     - id
- *     - name e.g. "Forge 1"
+/* Projects
+ *  - id
+ *  - name e.g. "Forge 1"
  */
 CREATE TABLE projects (
     id SERIAL PRIMARY KEY,
     name TEXT
 );
 
-/* - Files
- *     - id
- *     - student_id
- *     - project_id
- *     - name e.g. "forge1.rkt"
- *     - current_contents e.g. "#lang forge\n\n..."
+/* Files
+ *  - id
+ *  - student_id
+ *  - project_id
+ *  - name e.g. "forge1.rkt"
+ *  - current_contents e.g. "#lang forge\n\n..."
  */
 CREATE TABLE files (
     id SERIAL PRIMARY KEY,
@@ -44,11 +44,11 @@ CREATE TABLE diffs (
     diff TEXT
 );
 
-/* - Executions
- *     - id
- *     - file_id
- *     - time
- *     - mode ("forge", "forge/core", "forge/check-ex-spec", ...)
+/* Executions
+ *  - id
+ *  - file_id
+ *  - time
+ *  - mode ("forge", "forge/core", "forge/check-ex-spec", ...)
  */
 CREATE TABLE executions (
     id SERIAL PRIMARY KEY,
@@ -57,10 +57,10 @@ CREATE TABLE executions (
     mode TEXT
 );
 
-/* - Commands
- *     - id
- *     - execution_id
- *     - command e.g. "(run my-run #:preds [...])"
+/* Commands
+ *  - id
+ *  - execution_id
+ *  - command e.g. "(run my-run #:preds [...])"
  */
 CREATE TABLE commands (
     id SERIAL PRIMARY KEY,
@@ -68,28 +68,28 @@ CREATE TABLE commands (
     command TEXT
 );
 
-/* - Runs
- *     - command_id
- *     - result ("sat", "unsat")
+/* Runs
+ *  - command_id
+ *  - result ("sat", "unsat")
  */
 CREATE TABLE runs (
     command_id INTEGER REFERENCES commands(id),
     result TEXT
 );
 
-/* - Cores
- *     - command_id
- *     - core e.g. "x and y"
+/* Cores
+ *  - command_id
+ *  - core e.g. "x and y"
  */
 CREATE TABLE cores (
     command_id INTEGER REFERENCES commands(id),
     core TEXT
 );
 
-/* - Instances
- *     - id
- *     - command_id
- *     - model e.g. '{"A": ["A0", "A1", "A2"], ...}'::JSONB
+/* Instances
+ *  - id
+ *  - command_id
+ *  - model e.g. '{"A": ["A0", "A1", "A2"], ...}'::JSONB
  */
 CREATE TABLE instances (
     id SERIAL PRIMARY KEY,
