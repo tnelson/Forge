b160af553c2839dd822765ebd52d7e6865ec07bc
as130 <abigail_siegel@brown.edu>
2020-11-12 11:04:43 -0500

    work on lift-bounds

--- forge/amalgam/lift-bounds.rkt
+++ forge/amalgam/lift-bounds.rkt
@@ -111,9 +111,15 @@
     ; SET INTERSECTION
     [(? node/expr/op/&?)
      ;(printf "& ~a~n" expr)
-     (define children (map (lambda (x) (lift-bounds-expr x quantvars runContext)) args))
+     ;(define children (map (lambda (x) (lift-bounds-expr x quantvars runContext)) args))
      ; first argument of & struct is the arity, second is the child expressions
-     (node/expr/op/& (length children) children)
+
+     ; map to get the upper bounds
+     ; filter to filter our the LHS only if they are also in upper bounds of RHS
+     (define upper-bounds-LHS (lift-bounds-expr (first args) quantvars runContext))
+     (define upper-bounds-RHS (lift-bounds-expr (rest args) quantvards runContext))
+     (filter (lambda (x) (member? x upper-bounds-RHS)) upper-bounds-LHS)
+     ;(node/expr/op/& (length children) children)
      ]
 
     ; PRODUCT
