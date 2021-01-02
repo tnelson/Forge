e845996439c12f5e30f67f4ec99586832e0c5f8f
Lucia Reyes <lucia_reyes@brown.edu>
2021-01-02 17:13:48 -0600

    Made quick fix on quantifier case

--- forge/amalgam/tests/forge_ex2.rkt
+++ forge/amalgam/tests/forge_ex2.rkt
@@ -3,6 +3,6 @@
 sig Node {edges: set Node}
 
 pred irreflexive {    
-    --all n: Node | n not in n.^edges
+    -- all n: Node | n not in n.^edges
     not (some iden & edges) -- "no" isn't handled correctly by evaluation TODO
 }
