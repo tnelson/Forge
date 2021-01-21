bf57b9ec42db41a83f4ead8df33b22fcc72d45b8
Tim Nelson <tbnelson@gmail.com>
2021-01-21 07:35:19 -0500

    add dep, labels for discussion

--- forge/info.rkt
+++ forge/info.rkt
@@ -5,19 +5,21 @@
 (define version "0.8.0")
 
 (define deps '("base"
-               "syntax-classes"
-               "br-parser-tools-lib"
-               "brag-lib"
-               "net-lib"
-               "profile-lib"
-               "rackunit-lib"
-               "syntax-color-lib"
-               "web-server-lib"
-               "beautiful-racket"
-               "predicates"
-               "crypto-lib" ; ???
-               "mischief" ; for amalgam (stream cartesian product)
-               "gui-lib"  ; for syntax highlighting in tool
+               "syntax-classes"       ; used in parser and expander
+               "br-parser-tools-lib"  ; used in parser
+               "brag-lib"             ; used in parser
+               "beautiful-racket"     ; used in indenter
+               "syntax-color-lib"     ; used in syntax coloring for #lang forge
+               "net-lib"              ; ???
+               "profile-lib"          ; ???
+               "crypto-lib"           ; ???
+               "rackunit-lib"         ; used for Forge tests               
+               "web-server-lib"       ; used for Sterling server                                             
+               "mischief"             ; for amalgam (stream cartesian product)
+               "gui-lib"              ; for syntax highlighting in tool
+               "drracket-plugin-lib"  ; for tool
+               "pretty-format"        ; for occasional wrapping of large formulas
+               "predicates"           ; for occasional ease of defining boolean-valued functions
                ))
 
 ;(define scribblings '(("doc/quickstart.scrbl" ())))
