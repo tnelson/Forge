6f19d1f5c9e3c779c7a4c5df2f9d90abfce22e86
Lucia Reyes <lucia_reyes@brown.edu>
2021-01-21 10:13:34 -0500

    Added printlines, still haven't been able to find the bug

--- forge/amalgam.rkt
+++ forge/amalgam.rkt
@@ -3,9 +3,10 @@
 (set-verbosity 1)
 
 (require forge/amalgam/desugar/desugar)
+
 (require "amalgam/lift-bounds/lift-bounds.rkt")
 
-(require "amalgam/tests/forge_ex.rkt")
+;(require "amalgam/tests/forge_ex.rkt")
 (require "amalgam/userStudies/KittyBacon.rkt")
 (require racket/hash)
 (require (prefix-in @ racket/set))
@@ -18,11 +19,10 @@
 ;(require (only-in "breaks.rkt" sbound))
 ; ^ This will be a *different* struct defn; instead get via sigs
 
-(run udt
-     #:preds [isUndirectedTree]
-     ;#:preds [irreflexive]
-     #:scope [(Node 4)])
-
+; TODO: Comment this out later 
+;(run udt
+;     #:preds[isUndirectedTree]
+;     #:scope[(Node 4)])
 
 (run KB
      #:preds[KittyBaconIsCool]
@@ -259,7 +259,8 @@
   (run alt-run
        #:preds []
        #:bounds alt-inst)
-  
+  (printf "I'm right before the evaluate ~n")
+  (printf "Return value of evaluate is ~a~n" (not (evaluate alt-run 'unused F)))
   ; evaluate to see if tup is locally necessary
   (define result (not (evaluate alt-run 'unused F)))
   (forge:close-run alt-run)
@@ -288,6 +289,7 @@
                                                orig-run))
           (define curr
             (filter-map (lambda (pair)
+                          (printf "Current pair is ~a~n" pair)
                           (if (is-locally-necessary (cons pair name) orig-run)
                               (cons pair (forge:Relation-rel r))
                               #f))
@@ -402,7 +404,11 @@
 ; these are OK assuming 3, 4, 5, 6 are used
 ; add
 
-(define test_N1N1_edges (build-provenances (cons '(Node1 Node1) "edges") udt))
+; TODO: uncomment this later 
+;(define test_N1N1_edges (build-provenances (cons '(Node1 Node1) "edges") udt))
+
+(stream-first (forge:Run-result KB))
+;(define test_local_necessity_udt (get-locally-necessary-list udt))
 
 (define test_local_necessity_kittyBacon (get-locally-necessary-list KB))
 (printf "LOCAL NECESSITY TEST --- ~a --- " test_local_necessity_kittyBacon)
@@ -454,13 +460,14 @@
                                               los))) cartesian)]
     [else (error "build-nth-alphaset bad arg type" ptree)]))
 
-(require pretty-format)
-(pretty-printf "Tree is: ~a~nCount of provenances: ~a~n"
-               test_N1N1_edges
-               (count-provenances test_N1N1_edges))
-(define test_N1_N1_edges_pstream (build-alphaset-stream test_N1N1_edges))
-(pretty-printf "Alpha sets are: ~a~n"
-               (stream->list test_N1_N1_edges_pstream))
+; TODO: UNCOMMENT THIS LATER 
+;(require pretty-format)
+;(pretty-printf "Tree is: ~a~nCount of provenances: ~a~n"
+;               test_N1N1_edges
+;               (count-provenances test_N1N1_edges))
+;(define test_N1_N1_edges_pstream (build-alphaset-stream test_N1N1_edges))
+;(pretty-printf "Alpha sets are: ~a~n"
+;               (stream->list test_N1_N1_edges_pstream))
 
 ; Sketches how you might highlight the N^th provenance from a tree
 ; IMPORTANT NOTE: right now the highlighting system requires
