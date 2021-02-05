e3c599dffcf6caac6fc1212ab53295cf55d36ec6
Lucia Reyes <lucia_reyes@brown.edu>
2021-02-05 09:46:30 -0500

    Made changes to test-model2

--- forge/amalgam/userStudies/test-model2.rkt
+++ forge/amalgam/userStudies/test-model2.rkt
@@ -3,37 +3,41 @@
 option local_necessity on 
 sig WeHangUser {
     follows: set WeHangUser,
-    isVerified: one Bool,
-    hasProfilePic: one Bool
+    isVerified: one Bool
+    --hasProfilePic: one Bool
 }
 
 abstract sig Bool {} 
 one sig Yes extends Bool {}
 one sig No extends Bool {}
 
--- if weHangUserA follows weHangUserB, then weHangUserB must follow weHangUserA 
+-- (1) if weHangUserA follows weHangUserB, then weHangUserB must follow weHangUserA 
 pred friendship {
     all w1, w2: WeHangUser | w1 in w2.follows implies w2 in w1.follows
 }
 
--- weHang users can't follow each other 
+-- (2) weHang users can't follow themselves 
 pred noSelfFollower {
     no iden & follows
 }
 
--- weHang users are verified if they have more than 2 followers
--- because follows is the same as followers, we can use follows
--- weHang users can only be verified if they have a profile pic 
-
+-- (3) weHang users are verified if they have more than 2 followers
 pred verifiedWeHangUser {
-    all w:WeHangUser | ((#(w.follows) > 2) and w.hasProfilePic = Yes) iff w.isVerified = Yes 
+    --all w:WeHangUser | ((#(w.follows) > 2) and w.hasProfilePic = Yes) iff w.isVerified = Yes
+     all w:WeHangUser | (#(w.follows) > 2) iff w.isVerified = Yes 
 }
 
--- users can have no followes (we are not constrainint that they need followers)
+-- (4) If a user has a follow, then the user must have more than 1 follow
+pred leastNumFriends {
+    all w: WeHangUser | some w.follows iff #(w.follows) > 1
+}
+
+-- (5) users can have no followes (we are not constrainint that they need followers)
 
 pred weHangSimulation {
     friendship
     noSelfFollower
+    leastNumFriends
     verifiedWeHangUser
 }
 
