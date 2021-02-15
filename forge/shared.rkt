9f1339a75883b4de1e908616c19bacfb91f41238
Tim Nelson <tn@cs.brown.edu>
2021-02-15 16:08:51 -0500

    do version print in pipeline

--- forge/shared.rkt
+++ forge/shared.rkt
@@ -55,7 +55,3 @@
 (define (stream-map/once func strm)
   (stream-cons (func (stream-first strm))
                (stream-map/once func (stream-rest strm))))
-
-; Print version number, so students know to update
-(when (>= (get-verbosity) VERBOSITY_LOW)
-  (printf "Forge version: ~a~n" forge-version))
