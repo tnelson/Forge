0f7017486417322361ae0853e8a23166dd9f8624
FanC-06 <qianfan_chen@outlook.com>
2021-07-28 14:03:27 -0400

    delete <: :> node-op

--- forge/lang/ast.rkt
+++ forge/lang/ast.rkt
@@ -270,8 +270,8 @@
   (define-node-op id node/expr/op join-arity #:join? #t #:type node/expr?))
 (define-op/join join)
 
-(define-node-op <: node/expr/op get-second #:max-length 2 #:domain? #t #:type node/expr?)
-(define-node-op :> node/expr/op get-first  #:max-length 2 #:range? #t #:type node/expr?)
+;(define-node-op <: node/expr/op get-second #:max-length 2 #:domain? #t #:type node/expr?)
+;(define-node-op :> node/expr/op get-first  #:max-length 2 #:range? #t #:type node/expr?)
 (define-node-op ++ node/expr/op get-first #:same-arity? #t #:min-length 2 #:max-length 2 #:type node/expr?)
 (define-node-op sing node/expr/op (const 1) #:min-length 1 #:max-length 1 #:type node/int?)
 
