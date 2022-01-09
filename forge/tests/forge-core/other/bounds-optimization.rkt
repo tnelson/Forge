4dc4d0bd7ff9b7d0f421dc50c87de8a893270a3e
Tim Nelson <tbnelson@gmail.com>
2022-01-09 15:52:14 -0500

    Initial test case (not passing) for bounds optimization

---
+++ forge/tests/forge-core/other/bounds-optimization.rkt
@@ -0,0 +1,27 @@
+#lang forge/core
+
+(require (prefix-in @ rackunit))
+
+(sig Parent)
+(sig A #:extends Parent #:one)
+(sig B #:extends Parent #:one)
+
+; Create a Run, but don't open a visualizer
+(run myRun #:preds [])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; #:one sigs should have exactly one element in their upper bound.
+(@check-equal?
+ 1
+ (length (forge:bound-upper (first (filter (lambda (x) (equal? A (forge:bound-relation x))) (forge:Run-kodkod-bounds myRun))))))
+
+; #:one sigs should be exact-bounded
+(@check-equal?
+ (forge:bound-upper (first (filter (lambda (x) (equal? A (forge:bound-relation x))) (forge:Run-kodkod-bounds myRun))))
+ (forge:bound-lower (first (filter (lambda (x) (equal? A (forge:bound-relation x))) (forge:Run-kodkod-bounds myRun)))))
+
+; Upper bounds between #:one siblings should never overlap
+(@check-not-equal?
+ (forge:bound-upper (first (filter (lambda (x) (equal? A (forge:bound-relation x))) (forge:Run-kodkod-bounds myRun))))
+ (forge:bound-upper (first (filter (lambda (x) (equal? B (forge:bound-relation x))) (forge:Run-kodkod-bounds myRun)))))
