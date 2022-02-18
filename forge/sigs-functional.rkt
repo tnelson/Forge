69d4b1f6e22ba89f77973f9cda5e9ae76848951c
Thomas Del Vecchio <thomasdelvecchio4@gmail.com>
2022-02-17 19:57:38 -0500

    Fix bug with setting numerical scope in bounds; fix setting bitwidth in numerical bounds.

--- forge/sigs-functional.rkt
+++ forge/sigs-functional.rkt
@@ -259,7 +259,7 @@
 (define (safe-fast-eval-int-expr ie binding bitwidth)
   (-> node/int? hash? number? (listof (listof (or/c number? symbol?))))
   (define result (eval-int-expr ie binding bitwidth))
-  (de-integer-atomize result))
+  (caar (de-integer-atomize `((,result)))))
 
 
 (define/contract (do-bind bind scope bound)
@@ -300,9 +300,9 @@
      (match left
        [(ast:node/int/op/card c-info (list left-rel))
         (let* ([exact (safe-fast-eval-int-expr right (Bound-tbindings bound) 8)]
-               ; do we need the if (equal? (relation-name rel) "Int")
-               ; case that sigs.rkt has?
-               [new-scope (update-int-bound scope left-rel (Range exact exact))])
+               [new-scope (if (equal? (relation-name left-rel) "Int")
+                              (update-bitwidth scope exact)
+                              (update-int-bound scope left-rel (Range exact exact)))])
           (values new-scope bound))]
        [_ (fail)])]
 
