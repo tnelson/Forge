d005a875e265359284768637e420a3638aed5fba
Ben Greenman <benjaminlgreenman@gmail.com>
2022-10-20 22:02:02 -0400

    froglet info file

---
+++ froglet/info.rkt
@@ -0,0 +1,7 @@
+#lang info
+(define collection "froglet")
+(define deps '("base" "rackunit-lib" "forge"))
+(define build-deps '())
+(define pkg-desc "Conditional #lang")
+(define version "0.1")
+(define pkg-authors '(ben tbnelson))
