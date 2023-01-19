d72010d62e6c3f24f837bed16f029dfba87b51ec
Ben Greenman <benjaminlgreenman@gmail.com>
2023-01-19 13:18:48 -0500

    typed-froglet: install froglet for CI

--- froglet/info.rkt
+++ froglet/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "froglet")
 (define deps '("base" "rackunit-lib" "forge"))
+(define implies '("forge"))
 (define build-deps '())
-(define pkg-desc "Conditional #lang")
-(define version "0.1")
+(define version "0.2")
 (define pkg-authors '(ben tbnelson))
