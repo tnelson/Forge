ad57f6383ce0400c53a2f91c24c0b862ebfc711a
Ben Greenman <benjaminlgreenman@gmail.com>
2023-01-26 13:27:59 -0500

    typed-froglet: more typechecking, preds expect formulas

--- forge/tests/error/main.rkt
+++ forge/tests/error/main.rkt
@@ -70,19 +70,11 @@
     (list "froglet-reachable2.frg" #rx"not a singleton") ;; TODO
     (list "froglet-set-singleton-equal.frg" #rx"not a singleton") ;;(#rx"= expects two objects, sig Node is not an object")
     (list "froglet-set.frg" #rx"pred must return an object, not a set")
-<<<<<<< HEAD
     (list "froglet-uni-0.frg" #rx"expected a sig")
 ;    (list "froglet-uni-1.frg" #rx"expected a sig")
 ;    (list "froglet-uni-2.frg" #rx"is not a value") ;; TODO better block checking
 ;    (list "froglet-uni-3.frg" #rx"expected a sig")
 ;    (list "froglet-uni-4.frg" #rx"expected a sig") ;; TODO reachable, double-check
-=======
-    (list "froglet-uni-0.frg" #rx"Expected a sig")
-    (list "froglet-uni-1.frg" #rx"Expected a sig")
-    (list "froglet-uni-2.frg" #rx"is not a value") ;; TODO better block checking
-    (list "froglet-uni-3.frg" #rx"Expected a sig")
-    (list "froglet-uni-4.frg" #rx"Expected a sig") ;; TODO reachable, double-check
->>>>>>> d1177ee7... froglet: build syntax->type hash
 
     (list "example_electrum.frg" #rx"example foo: .* temporal")
     (list "example_impossible.frg" #rx"Invalid example 'onlyBabies'")
