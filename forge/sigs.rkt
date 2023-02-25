573ec952fa9c03d96704372c2100cb00d7508e0a
Ben Greenman <benjaminlgreenman@gmail.com>
2023-02-24 22:45:56 -0500

    inst, flatten

--- forge/sigs.rkt
+++ forge/sigs.rkt
@@ -425,10 +425,10 @@
 ; (inst name binding ...)
 (define-syntax (inst stx)
   (syntax-parse stx
-    [(inst name:id binds:expr)
+    [(inst name:id binds:expr ...)
      (syntax/loc stx
        (begin
-         (define name (make-inst binds))
+         (define name (make-inst (flatten (list binds ...))))
          (update-state! (state-add-inst curr-state 'name name))))]))
 
 ; Run a given spec
